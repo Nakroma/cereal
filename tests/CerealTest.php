7eded7dfb6a1c379323e393983150d867dfab5df
Nakroma <tarackobarack@gmail.com>
2017-03-15 19:08:59 +0100

    Added length unit test

--- tests/CerealTest.php
+++ tests/CerealTest.php
@@ -11,11 +11,25 @@ use PHPUnit_Framework_TestCase as Testcase;
  */
 class CerealTest extends TestCase
 {
+    /**
+     * Tests if two generated keys are unique
+     */
     public function testIsUnique()
     {
         $cereal1 = Cereal::generate();
         $cereal2 = Cereal::generate();
 
         $this->assertNotEquals($cereal1, $cereal2);
+    }
+
+    /**
+     * Tests if the string has the correct length
+     */
+    public function testCorrectLength()
+    {
+        $length = rand();
+        $key = Cereal::generate(['length' => $length, 'delimiter' => '']);
+
+        $this->assertEquals(strlen($key), $length);
     }
 }
